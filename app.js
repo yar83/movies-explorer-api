bd3ccf907c468afd15ad9fc01e96791a7fbaa2b6
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-03-22 21:43:23 +0300

    feat(linter): add liner, fix typos

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.IS_PROD
-  ? 'mongodb://127.0.0.1:27017/moviesdb'
-  : process.env.DB_URI
-);
+mongoose.connect(process.env.IS_PROD ? 'mongodb://127.0.0.1:27017/moviesdb' : process.env.DB_URI);
 
-console.log('I\'m ready to serve');
 app.listen(process.env.IS_PROD ? 3001 : 3000);
-
