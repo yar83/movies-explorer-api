9ff2a575f7b279912c57c10d7f71f282ef1459f0
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-03-22 21:28:32 +0300

    feat(app): add development env settings and first line of app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.IS_PROD
+  ? 'mongodb://127.0.0.1:27017/moviesdb'
+  : process.env.DB_URI
+);
+
+console.log('I\'m ready to serve');
+app.listen(process.env.IS_PROD ? 3001 : 3000);
+
