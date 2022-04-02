23ab4f4359c42e7f145300b5eaad5d2e1b91cbe8
Yaroslav Bashmakov <puer.robustus@gmail.com>
2022-04-02 16:26:09 +0300

    improvement(secKey): set secKey as constant from external file

--- routes/middlewares/auth/auth.js
+++ routes/middlewares/auth/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const CustomError = require('../../../utils/errors/CustomError');
 const errMsg = require('../../../utils/constants/errors');
+const { devMode } = require('../../../utils/constants/env');
 
 const auth = (req, res, next) => {
   const { token } = req.cookies;
@@ -13,7 +14,7 @@ const auth = (req, res, next) => {
       token,
       process.env.NODE_ENV === 'prod'
         ? process.env.SECRET_KEY
-        : 'not-so-secret',
+        : devMode.secKey,
     );
   } catch (err) {
     next(CustomError.getCustomError(401, errMsg.authRequired));
